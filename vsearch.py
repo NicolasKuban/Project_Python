6a17a33540f3112aa5d01b46546571bb29c6dc53
NicolasKuban <nicrad@yandex.ru>
2022-09-28 08:17:55 +0300

    Добавлена функция search4letters, которая ищет множество букв во множестве фразы. По умолчания множество букв это гласные латинского алфавита

--- vsearch.py
+++ vsearch.py
@@ -3,4 +3,13 @@ def search4vowels(phrase:str) -> set:
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
 
+def search4letters(phrase:set, letters:str='aeiou') -> set:
+    """Возращает множество букв из множества 'letters',
+       найденных в указанной фразе 'phrase'"""
+    return set(letters).intersection(set(phrase))
+
+
+
+
+    
 print(search4vowels("Hello"))
